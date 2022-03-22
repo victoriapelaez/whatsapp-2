9a3c97b0f4b99458136b90b159763f4a33080a47
Victoria <mpelaez@cifpfbmoll.eu>
2022-03-22 15:54:50 +0100

    prepare chat async data

--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -2,16 +2,22 @@ import Head from "next/head";
 import styled from "styled-components"
 import Sidebar from "../../components/Sidebar";
 import ChatScreen from "../../components/ChatScreen";
+import { async } from "@firebase/util";
+import { db, auth } from "../../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import getRecipientEmail from "../../utils/getRecipientEmail";
 
-function Chat() {
+function Chat({ chat, messages }) {
+    const [user] = useAuthState(auth);
+
     return (
         <Container>
             <Head>
-                <title>Chat</title>
+                <title>Chat with {getRecipientEmail(chat.users, user)}</title>
             </Head>
             <Sidebar />
             <ChatContainer>
-                <ChatScreen />
+                <ChatScreen chat={chat} messages={messages} />
             </ChatContainer>
         </Container>
     )
@@ -20,8 +26,50 @@ function Chat() {
 
 export default Chat
 
+export async function getServerSideProps(context) {
+    const ref = db.collection('chats').doc(context.query.id);
+
+    //Prep messages on the server
+    const messagesRes = await ref
+        .collection('messages')
+        .orderBy('timestamp', "asc")
+        .get();
+
+    const messages = messagesRes.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+    })).map(messages => ({
+        ...messages,
+        timestamp: messages.timestamp.toDate().getTime()
+    }));
+
+    //Prep the chats
+    const chatRes = await ref.get()
+    const chat = {
+        id: chatRes.id,
+        ...chatRes.data()
+    }
+
+    return {
+        props: {
+            messages: JSON.stringify(messages),
+            chat: chat,
+        },
+    };
+}
+
 const Container = styled.div`
 display: flex;
 `;
 
-const ChatContainer = styled.div``;
+const ChatContainer = styled.div`
+flex: 1;
+overflow: scroll;
+height: 100vh;
+
+::-webkit-scrollbar{
+    display: none;
+}
+-ms-overflow-style:none;
+scrollbar-width: none;
+`;
