17f8c216f800e01016503161ef4c44a86e5d4b90
Victoria <mpelaez@cifpfbmoll.eu>
2022-03-22 14:52:14 +0100

    create route chat id an initial individual chat component

--- components/Chat.js
+++ components/Chat.js
@@ -3,20 +3,25 @@ import styled from "styled-components";
 import { auth, db } from "../firebase";
 import getRecipientEmail from "../utils/getRecipientEmail";
 import { Avatar } from "@material-ui/core";
-
 import { useCollection } from "react-firebase-hooks/firestore";
+import { useRouter } from "next/router";
 
 function Chat({ id, users }) {
+    const router = useRouter();
     const [user] = useAuthState(auth);
     const [recipientSnapshot] = useCollection(
         db.collection('users').where('email', '==', getRecipientEmail(users, user))
     );
 
+    const enterChat = () => {
+        router.push(`/chat/${id}`)
+    }
+
     const recipient = recipientSnapshot?.docs?.[0]?.data()
     const recipientEmail = getRecipientEmail(users, user)
 
     return (
-        <Container>
+        <Container onClick={enterChat}>
             {recipient ? (
                 <UserAvatar src={recipient?.photoURL} />
             ) : (
---
+++ components/ChatScreen.js
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+function ChatScreen() {
+    return (
+        <Container>
+            
+        </Container>
+    )
+
+}
+
+export default ChatScreen
+
+const Container = styled.div`
+
+`;
