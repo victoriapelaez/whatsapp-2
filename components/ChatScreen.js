dd1f7ea9cb761ebe663cb953827398d43b4d8b75
Victoria <mpelaez@cifpfbmoll.eu>
2022-03-22 17:14:14 +0100

    add initial message component

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,9 +1,61 @@
 import styled from "styled-components"
+import { auth, db } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRouter } from "next/router";
+import { Avatar, IconButton } from "@material-ui/core";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import { useCollection } from "react-firebase-hooks/firestore";
+import Message from "./Message";
 
-function ChatScreen() {
+function ChatScreen({ chat, messages }) {
+    const [user] = useAuthState(auth);
+    const router = useRouter();
+    const [messagesSnapshot] = useCollection(
+        db
+            .collection('chats')
+            .doc(router.query.id)
+            .collection('messages')
+            .orderBy('timestamp', 'asc')
+    );
+
+    const showMessages = () => {
+        if (messagesSnapshot) {
+            return messagesSnapshot.docs.map(message => (
+                <Message
+                    key={message.id}
+                    user={message.data().user}
+                    message={{
+                        ...message.data(),
+                        timestamp: message.data().timestamp?.toDate().getTime(),
+                    }}
+                />
+            ))
+        }
+    }
+
     return (
         <Container>
-            
+            <Header>
+                <Avatar />
+                <HeaderInformation>
+                    <h3>Rec Email</h3>
+                    <p>Last Seen...</p>
+                </HeaderInformation>
+                <HeaderIcons>
+                    <IconButton>
+                        <AttachFileIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </HeaderIcons>
+            </Header>
+
+            <MessageContainer>
+                { showMessages()}
+                <EndOfMessage />
+            </MessageContainer>
         </Container>
     )
 
@@ -12,5 +64,43 @@ function ChatScreen() {
 export default ChatScreen
 
 const Container = styled.div`
+
+`;
+
+const Header = styled.div`
+position: sticky;
+background-color: white;
+z-index: 100;
+top: 0;
+display: flex;
+padding: 11px;
+height: 80px;
+align-items: center;
+border-bottom: 1px solid whitesmoke;
+`;
+
+const HeaderInformation = styled.div`
+margin-left: 15px;
+flex: 1;
+
+> h3 {
+    margin-bottom: 3px;
+}
+
+> p {
+    font-size: 14px;
+    color: gray;
+}
+
+`;
+const HeaderIcons = styled.div`
+
+`;
+
+const MessageContainer = styled.div`
+
+`;
+
+const EndOfMessage = styled.div`
 
 `;
