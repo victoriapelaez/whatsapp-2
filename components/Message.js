3b5a1bca862b2652358b5171a658974584b77f0c
Victoria <mpelaez@cifpfbmoll.eu>
2022-03-23 12:42:52 +0100

    add sender, receiver and message moment

--- components/Message.js
+++ components/Message.js
@@ -1,10 +1,24 @@
+import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components"
+import { auth } from "../firebase";
+import moment from "moment";
+import { Timestamp } from "firebase/firestore";
+
 
 function Message({ user, message }) {
-    return(
+    const [userLoggedIn] = useAuthState(auth);
+
+    const TypeOfMessage = user === userLoggedIn.email ? Sender : Reciever;
+
+    return (
         <Container>
-            <p>{message.message}</p>
-        </Container>
+            <TypeOfMessage>
+                {message.message}
+            {/* <Timestamp></Timestamp> */}
+            {message.timestamp ? moment(message.timestamp).format('LT') : '...'}
+               
+            </TypeOfMessage>
+        </Container >
     )
 }
 
@@ -12,4 +26,25 @@ export default Message
 
 const Container = styled.div`
 
+`;
+
+const MessageElement = styled.p`
+width: fit-content;
+padding: 15px;
+border-radius: 8px;
+margin: 10px;
+min-width: 60px;
+padding-bottom: 26px;
+position: relative;
+text-align: right;
+`;
+
+const Sender = styled(MessageElement)`
+margin-left: auto;
+background-color: #dcf8c6;
+`;
+
+const Reciever = styled(MessageElement)`
+text-align: left;
+background-color: whitesmoke;
 `;
