ce5b05b723ac28257d2546573a14b46603d4c2c2
Victoria <mpelaez@cifpfbmoll.eu>
2022-03-23 13:44:09 +0100

    fix Sidebar import error

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Sidebar from '../components/sidebar'
+import Sidebar from '../components/Sidebar'
 
 export default function Home() {
   return (
