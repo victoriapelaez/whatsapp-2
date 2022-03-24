08ab7efbb07c90faa3206922cc92699eee2f324b
Victoria <mpelaez@cifpfbmoll.eu>
2022-03-24 16:08:32 +0100

    initial function sign up

--- components/accountBox/signupForm.js
+++ components/accountBox/signupForm.js
@@ -2,6 +2,11 @@ import React, { useContext } from "react";
 import { BoldLink, BoxContainer, FormContainer, Input, MutedLink, SubmitButton } from "./common";
 import Marginer from "../marginer/index";
 import { AccountContext } from "./accountContext";
+import { auth } from "../../firebase";
+
+const signUp = () => {
+    /* auth.signInWithPopup(provider).catch(alert) */
+};
 
 export function SignupForm() {
     const { switchToSignin } = useContext(AccountContext);
@@ -14,7 +19,7 @@ export function SignupForm() {
                 <Input type="password" placeholder="Confirm Password" />
             </FormContainer>
             <Marginer direction="vertical" margin="1.4em" />
-            <SubmitButton type="submit">Sign Up</SubmitButton>
+            <SubmitButton type="submit" onClick={signUp}>Sign Up</SubmitButton>
             <Marginer direction="vertical" margin="1em" />
             <MutedLink href="#">Already have an account?
                 <BoldLink href="#" onClick={switchToSignin}>Sign in</BoldLink>
--- pages/login.js
+++ pages/login.js
@@ -27,7 +27,7 @@ function Login() {
             </Head>
 
             <LoginContainer>
-                <Logo src='http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png' />
+                
                 <Button onClick={signIn} variant="outlined">Sign in with Google</Button>
             </LoginContainer>
         </Container>
@@ -44,18 +44,3 @@ height: 100vh;
 background-color: whitesmoke;
 `; 
 
-const LoginContainer = styled.div`
-padding: 100px;
-display: flex;
-flex-direction: column;
-align-items: center;
-background-color: white;
-border-radius: 5px;
-box-shadow: 0px 4px 14px -3px rgba(0, 0, 0, 0.7);
-`;
-
-const Logo = styled.img`
-height: 200px;
-width: 200px;
-margin-bottom: 50px;
-`;
