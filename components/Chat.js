cb150c4cb77ce89d1fdc74cde5296d36e9c5d084
Victoria <mpelaez@cifpfbmoll.eu>
2022-03-29 13:51:18 +0200

    add close icon & style & initial delete function

--- components/Chat.js
+++ components/Chat.js
@@ -2,9 +2,13 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
 import { auth, db } from "../firebase";
 import getRecipientEmail from "../utils/getRecipientEmail";
-import { Avatar } from "@material-ui/core";
+import { Avatar, IconButton } from "@material-ui/core";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { useRouter } from "next/router";
+import CloseIcon from '@mui/icons-material/Close';
+import { doc, Firestore } from "firebase/firestore";
+import { Redirect } from "next";
+import { useEffect } from "react";
 
 function Chat({ id, users }) {
     const router = useRouter();
@@ -17,6 +21,27 @@ function Chat({ id, users }) {
         router.push(`/chat/${id}`)
     }
 
+    const handleDel = () => {
+        db.collection("chats")?.doc("doc.id")?.delete()
+            .then(() => {
+                console.log("Chat Borrado")
+            });
+        {/* <Redirect to="/chats" /> */}
+    }
+
+    
+
+     useEffect(() => {
+        db.collection('chats')
+            .get()
+            .then(querySnapshot => {
+                querySnapshot.forEach(doc => {
+                    const chatId = doc.id;
+                    console.log(chatId)
+                });
+            });
+    }, []); 
+
     const recipient = recipientSnapshot?.docs?.[0]?.data()
     const recipientEmail = getRecipientEmail(users, user)
 
@@ -28,11 +53,19 @@ function Chat({ id, users }) {
                 <UserAvatar>{recipientEmail[0]}</UserAvatar>
             )}
             {recipient ? (
-                <p>{recipient?.name}</p>
+                <p style={{marginRight:'auto'}}>{recipient?.name}</p>
             ) : (
-                <p>{recipientEmail}</p>
+                <p style={{marginRight:'auto'}}>{recipientEmail}</p>
             )}
+            {/* <IconButton onClick={() => handleDel(id)}
+                id={id}
+            > */}
 
+            <IconButton onClick={handleDel}
+            >
+                <CloseIcon style={{ fontSize: 10 }} />
+            </IconButton>
+            
         </Container>
     )
 }
@@ -42,6 +75,7 @@ export default Chat;
 const Container = styled.div`
 display: flex;
 align-items: center;
+justify-content:space-between;
 cursor: pointer;
 padding: 15px;
 word-break: break-word;
@@ -55,3 +89,4 @@ const UserAvatar = styled(Avatar)`
 margin: 5px;
 margin-right: 15px;
 `
+
