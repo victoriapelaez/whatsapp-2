11990a16324ee723e9536b14faaa69efae51a68f
Victoria <mpelaez@cifpfbmoll.eu>
2022-03-30 12:11:33 +0200

    fix: delete a chat

--- components/Chat.js
+++ components/Chat.js
@@ -6,9 +6,6 @@ import { Avatar, IconButton } from "@material-ui/core";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { useRouter } from "next/router";
 import CloseIcon from '@mui/icons-material/Close';
-import { doc, Firestore } from "firebase/firestore";
-import { Redirect } from "next";
-import { useEffect } from "react";
 
 function Chat({ id, users }) {
     const router = useRouter();
@@ -22,50 +19,38 @@ function Chat({ id, users }) {
     }
 
     const handleDel = () => {
-        db.collection("chats")?.doc("doc.id")?.delete()
+        db.collection("chats").doc(id).delete()
             .then(() => {
-                console.log("Chat Borrado")
+                console.log("Chat Borrado", id)
             });
-        {/* <Redirect to="/chats" /> */ }
+        router.push('/')
     }
 
-
-
-    useEffect(() => {
-        db.collection('chats')
-            .get()
-            .then(querySnapshot => {
-                querySnapshot.forEach(doc => {
-                    const chatId = doc.id;
-                    console.log(chatId)
-                });
-            });
-    }, []);
 
     const recipient = recipientSnapshot?.docs?.[0]?.data()
     const recipientEmail = getRecipientEmail(users, user)
 
     return (
-        <Container onClick={enterChat}>
-            {recipient ? (
-                <UserAvatar src={recipient?.photoURL} />
-            ) : (
-                <UserAvatar>{recipientEmail[0]}</UserAvatar>
-            )}
-            {recipient ? (
-                <p style={{ marginRight: 'auto' }}>{recipient?.name}</p>
-            ) : (
-                <p style={{ marginRight: 'auto' }}>{recipientEmail}</p>
-            )}
-            {/* <IconButton onClick={() => handleDel(id)}
-                id={id}
-            > */}
+        <Container >
+            <InfoContainer onClick={enterChat}>
+                {recipient ? (
+                    <UserAvatar src={recipient?.photoURL} />
+                ) : (
+                    <UserAvatar>{recipientEmail[0]}</UserAvatar>
+                )}
+                {recipient ? (
+                    <p style={{ marginRight: 'auto' }}>{recipient?.name}</p>
+                ) : (
+                    <p style={{ marginRight: 'auto' }}>{recipientEmail}</p>
+                )}
+            </InfoContainer>
 
-            <IconButton onClick={handleDel}
-            >
-                <CloseIcon style={{ fontSize: 10 }} />
-            </IconButton>
-
+            <CloseContainer>
+                <IconButton onClick={handleDel}
+                >
+                    <CloseIcon style={{ fontSize: 10 }} />
+                </IconButton>
+            </CloseContainer>
         </Container>
     )
 }
@@ -91,3 +76,10 @@ margin: 5px;
 margin-right: 15px;
 `
 
+const CloseContainer = styled.div`
+
+`
+
+const InfoContainer = styled.div`
+display: flex;
+`
