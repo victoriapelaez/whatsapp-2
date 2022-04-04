16da3d9539242ab97ae2b0795663ac9ddb9186a6
Victoria <mpelaez@cifpfbmoll.eu>
2022-04-04 12:56:54 +0200

    upload image to storage and chat collection

--- components/FormsCreateChats/FormUploadImage.js
+++ components/FormsCreateChats/FormUploadImage.js
@@ -6,15 +6,19 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import { Container, IconButton } from '@mui/material';
-import { db } from "../../firebase";
-import storage from "../../firebase"
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { auth, db } from '../../firebase'
+import firebase from "firebase/compat/app";
+import { useRouter } from "next/router";
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 export default function FormUploadImage() {
+    const [user] = useAuthState(auth);
     const [open, setOpen] = React.useState(false);
     const [image, setImage] = React.useState(null)
     const [progress, setProgress] = React.useState(0)
     const [caption, setCaption] = React.useState('')
-
+    const router = useRouter();
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -24,46 +28,76 @@ export default function FormUploadImage() {
         setOpen(false);
     };
 
-    const handleChange = (e) => {
+     const handleChange = (e) => {
         if (e.target.files[0]) {
             setImage(e.target.files[0])
         }
     }
 
-    const handleUpload = () => {
-        const uploadTask = storage(`images/${image.name}`).put(image)
-        uploadTask.on(
-            "state_changed",
+    const sendImage = (ImageUrl) => {
+        
+        db.collection('users').doc(user.uid).set({
+            lastSeen: firebase.firestore.FieldValue.serverTimestamp()
+        }), { merge: true }
+
+        db.collection('chats').doc(router.query.id).collection('images').add({
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            image: ImageUrl,
+            user: user.email,         
+        })
+    }
+
+
+    const archivoHandler = async (e) => {
+        const metadata = {
+            contentType: 'image/jpeg'
+        };
+        const archivo = e.target.files[0];
+        console.log(archivo)
+        const storageRef = getStorage();
+        console.log(storageRef)
+        const archivoPath = ref(storageRef, 'images/' + archivo.name)
+        console.log(archivoPath)
+        const uploadTask = uploadBytesResumable(archivoPath, archivo, metadata)
+        console.log(uploadTask);
+
+        uploadTask.on('state_changed',
             (snapshot) => {
                 const progress = Math.round(
                     (snapshot.bytesTransferred / snapshot.totalBytes) * 100
                 )
                 setProgress(progress)
+
+                console.log('Upload is ' + progress + '% done');
+                switch (snapshot.state) {
+                    case 'paused':
+                        console.log('Upload is paused');
+                        break;
+                    case 'running':
+                        console.log('Upload is running');
+                        break;
+                }
             },
             (error) => {
-                console.log(error);
-                alert(error.message)
+
+                switch (error.code) {
+                    case 'storage/unauthorized':
+                        break;
+                    case 'storage/canceled':
+                        break;
+                    case 'storage/unknown':
+                        break;
+                }
             },
             () => {
-                storage
-                    .ref("images")
-                    .child(image.name)
-                    .getDownloadURL()
-                    .then(url => {
-                        db.collection('images').add({
-                            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-                            caption: caption,
-                            ImageUrl: url,
-                        })
-
-                        setProgress(0);
-                        setCaption("");
-                        setImage(null)
-                    })
-            }
-        )
-
-    }
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    console.log('File available at', downloadURL);
+                    sendImage(downloadURL)
+                    alert('Imagen subida con éxito')
+                });
+            })
+        handleClose()
+    };
 
     return (
         <Container>
@@ -76,17 +110,15 @@ export default function FormUploadImage() {
                         Choose an image to upload
                     </DialogContentText>
                     <form>
-                        {/* <progress value={progress} max="100" /> */}
-                        <input type="text" placeholder="Enter a caption..." hidden onChange={e => setCaption(e.target.value)} value={caption}
-                            
-                        ></input>
-                        <input type="file" onChange={handleChange} />
+                        <progress value={progress} max="100" />
+                        <br></br>
+                        <input type="text" placeholder="Enter a caption..." hidden onChange={e => setCaption(e.target.value)} value={caption} />
+                        <input type="file" onChange={archivoHandler} />
+                        {/*  <Button onClick={handleClose} style={{ color: 'teal', borderColor: 'teal' }} variant="outlined">Cancel</Button>
+                    <Button onSubmit={archivoHandler} style={{ color: 'white', backgroundColor: 'teal' }} type="submit" variant="contained">Upload</Button>
+                 */}
                     </form>
                 </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} style={{ color: 'teal', borderColor: 'teal' }} variant="outlined">Cancel</Button>
-                    <Button onClick={handleUpload} style={{ color: 'white', backgroundColor: 'teal' }} type="submit" variant="contained">Upload</Button>
-                </DialogActions>
             </Dialog>
         </Container>
     );
