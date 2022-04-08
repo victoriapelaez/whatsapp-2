98ad46ac91e5e20e25202e11718ba506d35e058e
Victoria <mpelaez@cifpfbmoll.eu>
2022-04-08 12:36:12 +0200

    change Loading style

--- components/Loading.js
+++ components/Loading.js
@@ -6,13 +6,7 @@ function Loading() {
     return (
         <center style={{display: "grid", placeItems:"center", height:"100vh"}}>
             <div>
-                <img
-                    src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
-                    alt={'logo'}
-                    style={{ marginBottom: 10 }}
-                    height={200}
-                />
-                <Circle color="#3CBC28" size={60} />
+                <Circle color="#3CBC28" size={100} />
             </div>
         </center>
     );
